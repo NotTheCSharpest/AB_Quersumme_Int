11e32e0833cf909c3cd2f11123321faf724c4e80
NotTheCSharpest <63044477+NotTheCSharpest@users.noreply.github.com>
2020-06-15 12:20:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AB_Quersumme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // quersumme - int Aufgabe
+            Console.WriteLine("Bitte Ihre Quersumme eingeben");
+            string eingabe = Console.ReadLine();
+            int quersumme = 0;
+            string ziffer;
+            for (int i = 0; i < eingabe.Length; i++)
+            {
+                ziffer = Char.ToString(eingabe[i]);
+                //Console.WriteLine("ziffer ist jetzt: {0}", ziffer)
+;                quersumme = (quersumme + int.Parse(ziffer));
+                //Console.WriteLine("quersumme ist jetzt: {0}", quersumme);
+            }
+            Console.WriteLine("Ihre Quersumme ist: {0}", quersumme);
+
+        }
+    }
+}
